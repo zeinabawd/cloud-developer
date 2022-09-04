df98217fe52640922efaaaab761c561275fe4b75
Awad, Zeinab, Vodafone <zeinab.awad-mohamed@vodafone.com>
2022-09-04 14:03:29 +0200

    feat: add concat method

--- course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
+++ course-02/exercises/udacity-c2-basic-server/src/unit-test-examples/units.tests.ts
@@ -1,4 +1,4 @@
-import { add, divide } from './units';
+import { add, divide, concat } from './units';
 
 import { expect } from 'chai';
 import 'mocha';
@@ -38,3 +38,15 @@ describe('divide', () => {
 // @TODO try creating a new describe block for the "concat" method
 // it should contain an it block for each it statement in the units.ts @TODO.
 // don't forget to import the method ;)
+describe('concat', () => {
+
+  it('should concat Hello Zeinab', () => {
+    const result = concat("Hello","Zeinab");
+    expect(result).to.equal("Hello Zeinab");
+  });
+
+  it('should throw an error if strings are empty', () => {
+    expect(()=>{ concat("","") }).to.throw('strings are empty')
+  });
+
+});
